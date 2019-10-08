4f6e3621e1ae5a81931f780f041e11256f8eec53
AmeliaS5845 <56265179+AmeliaS5845@users.noreply.github.com>
2019-10-08 11:17:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Amelia Sambone"
+let userName = "AmeliaS5845";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
